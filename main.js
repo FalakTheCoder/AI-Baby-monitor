cbcfcd53005146b048c2499998c7b54a1e3ed946
FalakTheCoder <92030342+FalakTheCoder@users.noreply.github.com>
2022-04-12 17:56:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,18 @@
-objectDetector= "";
 
-img = "";
+song = "";
 objects = [];
 status = "";
 
-function preload(){
-  img = loadImage('dog_cat.jpg');
-  audio = document.getElementById("audio")
+function preload()
+{
+	song = loadSound("TLTSRXB_alert.mp3");
 }
-
 
 function setup() {
   canvas = createCanvas(380, 380);
   canvas.center();
   video = createCapture(VIDEO);
-  video.size(380, 380);
+  video.size(380,380);
   video.hide();
   objectDetector = ml5.objectDetector('cocossd', modelLoaded);
   document.getElementById("status").innerHTML = "Status : Detecting Objects";
@@ -23,7 +21,6 @@ function setup() {
 function modelLoaded() {
   console.log("Model Loaded!")
   status = true;
-  objectDetector.detect(video, gotResult);
 }
 
 function gotResult(error, results) {
@@ -32,35 +29,46 @@ function gotResult(error, results) {
   }
   console.log(results);
   objects = results;
-  if(results = person){
-    console.log("person detected");
-    document.getElementById("status").innerHTML = "Baby Detected";
-    audio.stop()
-  }else{
-    document.getElementById("status").innerHTML = "Baby not Detected";
-    audio.play()
-  }
 }
 
 
 function draw() {
   image(video, 0, 0, 380, 380);
-
       if(status != "")
       {
-        r = random(255);
-        g = random(255);
-        b = random(255);      
-        for (var i = 0; i < objects.length; i++) {
+        r =  random(255);
+        g =  random(255);
+        b =  random(255);      
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i < objects.length; i++) {
           document.getElementById("status").innerHTML = "Status : Object Detected";
-          document.getElementById("number_of_objects").innerHTML = "Number of Objects detected:" + objects.length;
-    
-          fill(r, g, b);
+ 
+          fill(r,g,b);
           percent = floor(objects[i].confidence * 100);
           text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
           noFill();
-          stroke(r, g, b);
+          stroke(r,g,b);
           rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+         
+          if(objects[i].label == "person")
+          {
+            document.getElementById("number_of_objects").innerHTML = "Baby Found";
+            console.log("stop");
+            song.stop();
+          }
+          else
+          {
+            document.getElementById("number_of_objects").innerHTML = "Baby Not Found";
+            console.log("play"); 
+            song.play();
+          }
+         }
+
+        if(objects.length == 0)
+        {
+          document.getElementById("number_of_objects").innerHTML = "Baby Not Found";
+          console.log("play"); 
+          song.play();
         }
       }
 }
